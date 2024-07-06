18de32a8b99771e7f5b6d8cb56bfdcb2c9fdd343
Ripon-Mardy <mardyripon10@gmail.com>
2024-07-06 22:35:47 +0600

    chage

--- app/product/page.js
+++ app/product/page.js
@@ -3,21 +3,28 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 
 
+import Product_Details from '@/components/Product_Details';
+import GetAQuoteForm from '@/components/Get_a_quote_form';
+import ProductExplore from '@/components/ProductExplore';
+
+
 // === icons ===
 import { TiWorld } from "react-icons/ti";
 import { FaNoteSticky } from "react-icons/fa6";
-import GetAQuoteForm from '@/components/Get_a_quote_form';
-import ProductExplore from '@/components/ProductExplore';
 import Product from '@/components/Product';
-import Product_Details from '@/components/Product_Details';
+import { FaAngleDown } from "react-icons/fa";
+
 
 const page = () => {
 
     // const text = "These 4-wheel electric forklifts is ready to face some of the most difficult material handling tasks. Max-8 series is your multipurpose workhorse that supplies great power, easy operation and leading performance to be the best solution for whatever you are lifting."
 
-    const text = <Product_Details/>
+    const text = <Product_Details />
 
     const [content, setContent] = useState(text);
+    const [isCategory, setIsCategory] =  useState(false);
+
+
 
 
 
@@ -28,7 +35,11 @@ const page = () => {
     }
 
     const handleQuoteButton = () => {
-        setContent(<GetAQuoteForm/>)
+        setContent(<GetAQuoteForm />)
+    }
+
+    const handleProductCategory = () => {
+        setIsCategory(!isCategory)
     }
 
     return (
@@ -50,23 +61,75 @@ const page = () => {
 
             <div className='container mx-auto py-10 px-3 md:px-0 '>
 
-                <div className='md:flex md:gap-10'>
-                    <div className=' w-4/5 mx-auto md:w-[35%]'>
+                <div>
 
-                        <Product/>
+                    <div className='md:flex md:gap-10'>
+                        <div className='md:w-1/4 md:flex flex-col gap-6 hidden'>
+                            <div className='md:flex flex-col relative justify-between border-b border-gray-300 pb-2 cursor-pointer font-medium'>
+                               <div onClick={handleProductCategory} className='flex items-center justify-between'>
+                               <h1>Rental Equipment</h1>
+                               <FaAngleDown />
+                               </div>
 
+                              <div>
+                              {
+                                isCategory && (
+                                    <div className='flex flex-col bg-gray-200 rounded-md gap-3 p-1 mt-2'>
+                                    <Link href={'#'} className='hover:bg-gray-300 p-1'>Forklift</Link>
+                                    <Link href={'#'} className='hover:bg-gray-300 p-1'>excavator</Link>
+                                    <Link href={'#'} className='hover:bg-gray-300 p-1'>Wheel loader</Link>
+                                    <Link href={'#'} className='hover:bg-gray-300 p-1'>payloader</Link>
+                                    <Link href={'#'} className='hover:bg-gray-300 p-1'>Road Roller</Link>
+                                </div>
+                                )
+                              }
+                              </div>
+                            </div>
+
+                           
+
+                            <div className='flex items-center justify-between border-b border-gray-300 pb-2 cursor-pointer font-medium'>
+                                <h1>Filters</h1>
+                                <FaAngleDown />
+                            </div>
+
+                            <div className='flex items-center justify-between border-b border-gray-300 pb-2 cursor-pointer font-medium'>
+                                <h1>Hydraulic parts</h1>
+                                <FaAngleDown />
+                            </div>
+
+                            <div className='flex items-center justify-between border-b border-gray-300 pb-2 cursor-pointer font-medium'>
+                                <h1>Transmission Parts</h1>
+                                <FaAngleDown />
+                            </div>
+
+                            <div className='flex items-center justify-between border-b border-gray-300 pb-2 cursor-pointer font-medium'>
+                                <h1>Brakes parts</h1>
+                                <FaAngleDown />
+                            </div>
+
+
+                        </div>
+                        <div className=' w-4/5 mx-auto md:w-[35%]'>
+
+                            <Product />
+
+                        </div>
+
+
+                        <div className='md:w-1/2'>
+                            <h1 className='text-3xl font-semibold mt-5 md:mt-0 mb-5'>1.5-3.5T Electric Forklift</h1>
+                            <p className='text-lg'>Max-8 series can be applied in heavy loads (up to 3,5t), such as beverage industry and building materials.</p>
+                        </div>
                     </div>
 
 
-                    <div className='md:w-1/2'>
-                        <h1 className='text-3xl font-semibold mt-5 md:mt-0 mb-5'>1.5-3.5T Electric Forklift</h1>
-                        <p className='text-lg'>Max-8 series can be applied in heavy loads (up to 3,5t), such as beverage industry and building materials.</p>
-                    </div>
                 </div>
 
 
+
                 <div>
-                    <div className='flex gap-4 mt-5'>
+                    <div className='flex gap-4 mt-10'>
                         <button onClick={() => handleProductButton("Details")} className="relative px-3 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
         before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 capitalize font-semibold flex items-center justify-center gap-2"> <TiWorld />  Product Details  </button>
 
@@ -76,7 +139,7 @@ const page = () => {
 
 
                     <div>
-                        <div className='mt-4 md:text-lg'>{content}</div>
+                        <div className='mt-7 md:text-lg'>{content}</div>
                     </div>
 
 
@@ -87,12 +150,12 @@ const page = () => {
 
 
             {/* ==== explore product ====  */}
-            <div className='container mx-auto py-6 px-3 md:px-0'>
+            <div className='container mx-auto py-20 px-3 md:px-0'>
 
                 <h1 className='text-2xl font-semibold'>Explore More Product</h1>
 
                 <div className='mt-3 opacity-100'>
-                  <ProductExplore/>
+                    <ProductExplore />
 
                 </div>
 
