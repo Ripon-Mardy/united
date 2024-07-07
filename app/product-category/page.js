9e99cf335724b63c212bab1f70cef7c6bfc73f24
Ripon-Mardy <mardyripon10@gmail.com>
2024-07-07 14:39:48 +0600

    change

--- app/product-category/page.js
+++ app/product-category/page.js
@@ -1,9 +1,9 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 // ==== image === 
-import product1 from './../../public/Image/product-category/p1.png'
 import product2 from './../../public/Image/product-category/p2.png'
 import product3 from './../../public/Image/product-category/p3.png'
 import product4 from './../../public/Image/product-category/p4.png'
@@ -16,7 +16,46 @@ import { FaArrowRight } from "react-icons/fa6";
 import { FaAnglesRight } from "react-icons/fa6";
 
 
+
+
+
 const page = () => {
+
+    const productList = [
+        {
+            "product_image" : product2,
+            "Product_title" : "1.5-3.5T Electric Forklift",
+            "product_content" : "These 4-wheel electric forklifts is ready to face some of the most difficult material handling tasks. Max-8 series is your […]"
+        },
+        {
+            "product_image" : product3,
+            "Product_title" : "1.5T Three Wheel Electric Forklift",
+            "product_content" : "EP introduces a compact and agile 3-wheel Li-ion forklift truck for the rapid turnover of goods in confined space with […]"
+        },
+        {
+            "product_image" : product4,
+            "Product_title" : "7.0-10.0T Lithium Powered Electric Forklift",
+            "product_content" : "EFL Series releases a new generation, EFL702/1002 7.0/10.0Ton Li-ion electric forklift, to fill the vacancy of large ton and send […]"
+        },
+        {
+            "product_image" : product5,
+            "Product_title" : "LPG/Gasoline Forklift – 2.5T",
+            "product_content" : "Introduction EQUIPMAX 2.5T Cushion Tires LPG forklift adopts the newest technologies and ergonomic concepts in designing and manufacturing. The forklift […]"
+        },
+        {
+            "product_image" : product6,
+            "Product_title" : "Telescopic Forklift 2WD/4WD – 2.5-4.0T",
+            "product_content" : "Introduction: The EQUIPMAX telescopic forklift (telehandler) adopts the latest technologies and ergonomic concepts in designing and manufacturing. The forklift with […]"
+        },
+        {
+            "product_image" : product7,
+            "Product_title" : "Stand-on VNA Three Way Forklift 1.0-1.5T",
+            "product_content" : "1.0-1.5T Stand-on VNA Three Way Forklift  "
+        }
+    ]
+
+   
+
     return (
         <>
             <div className=' productImageCover py-12 md:py-28'>
@@ -52,82 +91,20 @@ const page = () => {
                 {/* === product list === */}
                 <div className='py-16 flex flex-col items-start justify-center gap-10'>
 
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product1} className='md:w-1/2'></Image>
+                    {
+                        productList.map((product, index) => (
+                            <div key={index} className='md:flex md:gap-10 md:items-center md:justify-center'>
+                        <Image width={300} height={300} src={product.product_image} className='md:w-80 md:h-80 object-cover'></Image>
                         <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>7.0-10.0T Lithium Powered Electric Forklift</h1>
-                            <p className='md:text-lg mt-3'>EFL Series releases a new generation, EFL702/1002 7.0/10.0Ton Li-ion electric forklift, to fill the vacancy of large ton and send […]</p>
+                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'> {product.Product_title} </h1>
+                            <p className='md:text-lg mt-3'> {product.product_content} </p>
                             <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
         before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
                         </div>
                     </div>
+                        ))
+                    }
 
-
-                    
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product2} className='md:w-1/2'></Image>
-                        <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>LPG/Gasoline Forklift – 2.5T</h1>
-                            <p className='md:text-lg mt-3'>Introduction EQUIPMAX 2.5T Cushion Tires LPG forklift adopts the newest technologies and ergonomic concepts in designing and manufacturing. The forklift […]</p>
-                            <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
-        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
-                        </div>
-                    </div>
-
-
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product3} className='md:w-1/2'></Image>
-                        <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>Telescopic Forklift 2WD/4WD – 2.5-4.0T</h1>
-                            <p className='md:text-lg mt-3'>Introduction: The EQUIPMAX telescopic forklift (telehandler) adopts the latest technologies and ergonomic concepts in designing and manufacturing. The forklift with […]</p>
-                            <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
-        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
-                        </div>
-                    </div>
-
-
-
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product5} className='md:w-1/2'></Image>
-                        <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>1.5T Three Wheel Electric Forklift</h1>
-                            <p className='md:text-lg mt-3'>EP introduces a compact and agile 3-wheel Li-ion forklift truck for the rapid turnover of goods in confined space with […]</p>
-                            <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
-        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
-                        </div>
-                    </div>
-
-
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product7} className='md:w-1/2'></Image>
-                        <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>1.5-3.5T Electric Forklift</h1>
-                            <p className='md:text-lg mt-3'>These 4-wheel electric forklifts is ready to face some of the most difficult material handling tasks. Max-8 series is your […]</p>
-                            <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
-        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
-                        </div>
-                    </div>
-
-
-                    <div className='md:flex md:gap-10 md:items-center md:justify-center'>
-                        <Image width={320} height={320} src={product4} className='md:w-1/2'></Image>
-                        <div>
-                            <h1 className='md:text-3xl text-2xl mt-4 font-semibold'>Stand-on VNA Three Way Forklift 1.0-1.5T</h1>
-                            <p className='md:text-lg mt-3'>1.0-1.5T Stand-on VNA Three Way Forklift    </p>
-                            <button class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border border-gray-500
-        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-navBg hover:text-white hover:duration-1000 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 font-semibold mt-4 flex items-center gap-2 uppercase">more <FaArrowRight/> </button>
-                        </div>
-                    </div>
-
-                   
-
-
-                   
-
-
-                    
-
-                   
 
                    
 
